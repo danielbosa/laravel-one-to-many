0a48b8c8ea169ebfcdb76c40458c2b6b60888771
danielbosa <danielbosa95@gmail.com>
2024-06-07 01:11:25 +0200

    Validation for Type

---
+++ app/Http/Requests/StoreTypeRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StoreTypeRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => [
+                'required',
+                'max:200',
+                Rule::unique('types')->ignore($this->type),
+            ],
+            'icon' => 'nullable'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'Il nome è obbligatorio!',
+            'name.max' => 'Il nome deve essere lungo massimo :max caratteri!',
+            'name.unique' => 'Il nome deve essere univoco!',
+        ];
+    }
+}
