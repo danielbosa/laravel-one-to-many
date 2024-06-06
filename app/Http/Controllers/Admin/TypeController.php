b05de519be769fe79a1a5fbe494ce87dac1577a7
danielbosa <danielbosa95@gmail.com>
2024-06-07 01:10:32 +0200

    Crud functionality

--- app/Http/Controllers/Admin/TypeController.php
+++ app/Http/Controllers/Admin/TypeController.php
@@ -2,8 +2,17 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Http\Controllers\Controller;
+
 use App\Models\Type;
+
+
 use Illuminate\Http\Request;
+
+use App\Http\Requests\StoreTypeRequest;
+use App\Http\Requests\UpdateTypeRequest;
+
+use Illuminate\Support\Facades\Storage;
 
 class TypeController extends Controller
 {
@@ -12,7 +21,8 @@ class TypeController extends Controller
      */
     public function index()
     {
-        //
+        $types = Type::all();
+        return view('admin.types.index', compact('types'));
     }
 
     /**
@@ -20,15 +30,23 @@ class TypeController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.types.create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreTypeRequest $request)
     {
-        //
+        $form_data = $request->validated();
+        if($request->hasFile('image')){
+            $name = $request->image ->getClientOriginalName();
+            $path = Storage::putFileAs('post_images', $request->image, $name);
+            $form_data['image'] = $path;
+        };
+        $form_data['slug'] = Type::generateSlug($form_data['name']);
+        $newType = Type::create($form_data);
+        return redirect()->route('admin.types.show', $newType->slug);
     }
 
     /**
@@ -36,7 +54,7 @@ class TypeController extends Controller
      */
     public function show(Type $type)
     {
-        //
+        return view('admin.types.show', compact('type'));
     }
 
     /**
@@ -44,7 +62,7 @@ class TypeController extends Controller
      */
     public function edit(Type $type)
     {
-        //
+        return view('admin.types.edit', compact('type'));
     }
 
     /**
@@ -52,7 +70,26 @@ class TypeController extends Controller
      */
     public function update(Request $request, Type $type)
     {
-        //
+        $form_data = $request->all();
+        //$form_data['user_id'] = Auth::id();
+        //se il titolo è diverso, allora aggiorno anche lo slug
+        if ($type->name !== $form_data['name']) {
+            $form_data['slug'] = Type::generateSlug($form_data['name']);
+        }
+        if ($request->hasFile('image')) {
+            if ($type->icon) {
+                Storage::delete($type->icon);
+            }
+            $name = $request->image->getClientOriginalName();
+            //dd($name);
+            $path = Storage::putFileAs('post_images', $request->image, $name);
+            $form_data['icon'] = $path;
+        }
+        // DB::enableQueryLog();
+        $type->update($form_data);
+        // $query = DB::getQueryLog();
+        // dd($query);
+        return redirect()->route('admin.types.show', $type->slug);
     }
 
     /**
