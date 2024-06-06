25d743f9b2be5b7b5a2ad226950523b038068372
danielbosa <danielbosa95@gmail.com>
2024-06-06 19:09:59 +0200

    Seeding db from json

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Type;
 
 class TypeSeeder extends Seeder
 {
@@ -12,6 +13,20 @@ class TypeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $data = json_decode(file_get_contents(__DIR__ . '/data/type_db.json'), true);
+
+        foreach ($data as $type) {
+            
+            // Qui avrei potuto usare Type::create perché nel model ho specificato la proprietà fillable; MA devo generare lo slug tramite metodo --> quindi non posso, devo fare campo per campo
+            //Type::create($type);
+
+            
+            $newType = new Type;
+            $newType->name = $type['name'];
+            $newType->icon = $type['icon'];
+            $newType->slug = Type::generateSlug($newType->name);
+            $newType->save();
+
+        }
     }
 }
